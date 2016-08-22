5073113288134e40722353d020e042494eeab1ab
george udosen <datameshprojects@gmail.com>
2016-08-22 10:26:34 +0100

    Linked List.
    Creating linked list from node class, append method finished

---
+++ basic-datastructures/node/node.py
@@ -0,0 +1,91 @@
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+    def append(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getNext() == None:
+                ne = Node(item)
+                current.setNext(ne)
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def insert(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+
+mylist = UnorderedList()
+mylist.add(31)
+mylist.add(77)
+mylist.add(80)
+mylist.append(20)
+
