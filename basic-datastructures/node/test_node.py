336cbf991d9ef8cbfdd2c99abd384afc0b068e6c
george udosen <datameshprojects@gmail.com>
2016-08-22 23:45:03 +0100

    Linked List.
    Tests created.

---
+++ basic-datastructures/node/test_node.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestNode(TestCase):
+    def test_getData(self):
+        self.fail()
+
+    def test_getNext(self):
+        self.fail()
+
+    def test_setData(self):
+        self.fail()
+
+    def test_setNext(self):
+        self.fail()
---
+++ basic-datastructures/node/test_unorderedList.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+
+class TestUnorderedList(TestCase):
+    def test_isEmpty(self):
+        self.fail()
+
+    def test_add(self):
+        self.fail()
+
+    def test_size(self):
+        self.fail()
+
+    def test_search(self):
+        self.fail()
+
+    def test_remove(self):
+        self.fail()
+
+    def test_append(self):
+        self.fail()
+
+    def test_insert(self):
+        self.fail()
+
+    def test_pop(self):
+        self.fail()
+
+    def test_index(self):
+        self.fail()
