967eda6cedd410a08e8e5afc3c524f92c94a61c6
george udosen <datameshprojects@gmail.com>
2016-08-24 19:25:04 +0100

    helper file delete

---
+++ basic-datastructures/discuss_question_226/f1.py
@@ -0,0 +1,19 @@
+from pythonds.basic.stack import Stack
+
+
+def divideBy2(decNumber):
+    remstack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % 2
+        remstack.push(rem)
+        decNumber //= 2
+
+    binString = ""
+    while not remstack.isEmpty():
+        binString += str(remstack.pop())
+
+    return binString
+
+
+print(divideBy2(42))
