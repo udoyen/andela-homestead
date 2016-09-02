c47ae0b628c1496e4b5733507333e57e614020a6
george udosen <datameshprojects@gmail.com>
2016-09-02 19:15:06 +0100

    Recursive
    word reverse recursive function complete

--- recursion/test.py
+++ recursion/test.py
@@ -1,33 +1,8 @@
-# def toStr(n, base):
-#     convertString = "0123456789ABCDEF"
-#     if n < base:
-#         return convertString[n]
-#     else:
-#         return toStr(n // base, base) + convertString[n % base]
-#
-#
-# print(toStr(1453, 16))
-
-
-# def rev(word):
-#     l = list(word)
-#     newls = []
-#     if len(word) == 1:
-#         return "".join(newls.append(word))
-#     else:
-#         r = l.pop()
-#         newls.append(r)
-#         return rev("".join(l))
-#
-# print(rev("abcd"))
-
 def rev(word):
-    l = list(word)
-    for i in range(0, len(word)):
-        for j in range(0, 1):
-            l.insert(0, l.pop())
-        l.append(l.pop())
-        word = "".join(l)
-    return rev(word)
+    if len(word) == 1:
+        return word
+    else:
+        clip = rev(word[:-1])
+        return rev(word[-1:]) + clip
 
-print(rev("loop"))
+print(rev("apple"))
