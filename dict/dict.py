1b8b3a0953c01b07a7e3b018a52ee9af72180c37
george udosen <datameshprojects@gmail.com>
2016-10-15 19:46:34 +0100

    Dictionary
    Create dict within another

---
+++ dict/dict.py
@@ -0,0 +1,10 @@
+r = {'jack': [2, 3, 4, 5], 'paul': [1, 2, 3, 4, 5]}
+
+t = {
+    'jack': {'age': 34, 'sex': 'male', 'occupation': 'dentist'},
+    'paul': {'age': 45, 'sex': 'male', 'occupation': 'lawyer'},
+    'koko': {'age': 32, 'sex': 'male', 'occupation': 'doctor'},
+    'jane': {'age': 30, 'sex': 'female', 'occupation': 'developer'}
+}
+
+print(t['jack']['sex'])
